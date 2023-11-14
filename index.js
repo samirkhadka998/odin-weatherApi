e12bee3c564144b4be27d6b207fe0eeff20b5e78
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-11-14 22:27:54 +0545

    Stying element , design changes

--- index.js
+++ index.js
@@ -5,13 +5,15 @@
 const APIToken = '3a2e00bc69084173884141902232204'
 const upsplashaccessKey = 'D1dP_r4ksoL1wNqv401fzU7w3iPDsVGS9taVR6u01Vw';
 
+let wrapper = document.querySelector('.wrapper');
+
 
 LoadNodes();
 
 function LoadNodes() {
   let btn = document.querySelector('button');
   let search = document.querySelector('input');
-
+  wrapper.hidden = true;
   btn.addEventListener('click', LoadSearch)
   search.addEventListener('keyup', LoadSearch)
 }
@@ -22,21 +24,29 @@ function LoadSearch(e) {
   if (e.type === "click" || (e.code && e.type === "keyup" && e.code === "Enter")) {
     console.log(e)
     let search = document.querySelector('input');
-    let wrapper = document.querySelector('.wrapper');
     if (!search.value) return;
-
+    wrapper.hidden = false;
     let promise = WeatherApi(search.value);
     promise.then(res => {
       if (res.error) {
-        wrapper.textContent = res.error?.message;
+        wrapper.firstElementChild.textContent = '';
+        wrapper.lastElementChild.textContent = res.error?.message;
       }
       else {
         console.log(res)
-        wrapper.textContent = JSON.stringify(res);
+        wrapper.firstElementChild.textContent = res.location.tz_id + " - " + res.location.country;
+        wrapper.lastElementChild.textContent = res.current.condition.text;
+        let img = document.createElement('img');
+        img.alt = "weather current icon";
+        // "icon": "//cdn.weatherapi.com/weather/64x64/night/116.png",
+
+        img.src = 'https:' + '//cdn.weatherapi.com/weather/64x64/night/116.png'
+        wrapper.lastElementChild.appendChild(img);
         let uplashApiPromise = UplashApi(res.current.condition.text);
         uplashApiPromise.then(result => {
           if (result.error) {
-            wrapper.textContent = result.error?.message;
+            wrapper.firstElementChild.textContent = '';
+            wrapper.lastElementChild.textContent = result.error?.message;
           }
           else {
             // document.body.style.background ="url(https://images.unsplash.com/photo-1628018257710-753a94fc106a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1Mjc3ODN8MHwxfHJhbmRvbXx8fHx8fHx8fDE2OTk5NzMzOTZ8&ixlib=rb-4.0.3&q=80&w=1080)"
